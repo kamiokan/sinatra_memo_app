0c8043a1d539af4deffa8b632d13bc41b468cff3
kamiokan <spherenov@gmail.com>
2022-02-16 12:32:01 +0900

    メモIDが固定になるよう修正した

--- myapp.rb
+++ myapp.rb
@@ -5,6 +5,7 @@ require 'csv'
 
 before do
   @file_name = 'data.csv'
+  @id_file_name = 'id.txt'
   @memo = { id: '', title: '', body: '' }
 end
 
@@ -28,11 +29,13 @@ get '/memo/new' do
 end
 
 post '/memo/new' do
+  id = fetch_current_id
   title = params[:title]
   body = params[:body]
   CSV.open(@file_name, 'a') do |f|
-    f << [title, body]
+    f << [id, title, body]
   end
+  increment_id(id)
   redirect '/'
 end
 
@@ -53,8 +56,12 @@ patch '/memo/:id' do
   new_title = params[:title]
   new_body = params[:body]
   books = CSV.read(@file_name)
-  books[memo_id][0] = new_title
-  books[memo_id][1] = new_body
+  books = books.each do |book|
+    if book[0].to_i == memo_id
+      book[1] = new_title
+      book[2] = new_body
+    end
+  end
   File.delete(@file_name)
   books.each do |data|
     CSV.open(@file_name, 'a') do |f|
@@ -67,7 +74,9 @@ end
 delete '/memo' do
   memo_id = params[:id].to_i
   books = CSV.read(@file_name)
-  books.delete_at(memo_id)
+  books.each do |book|
+    book[0].to_i == memo_id && books.delete(book)
+  end
   File.delete(@file_name)
   books.each do |data|
     CSV.open(@file_name, 'a') do |f|
@@ -83,6 +92,25 @@ end
 
 def find_memo_by_id(memo_id)
   books = CSV.read(@file_name)
-  @memo[:title] = books[memo_id][0]
-  @memo[:body] = books[memo_id][1]
+  target_memo = books.find do |book|
+    book[0].to_i == memo_id
+  end
+  @memo[:title] = target_memo[1]
+  @memo[:body] = target_memo[2]
+end
+
+def fetch_current_id
+  if File.exist?(@id_file_name)
+    File.open(@id_file_name, 'r') do |f|
+      f.gets.to_i
+    end
+  else
+    1
+  end
+end
+
+def increment_id(id)
+  File.open(@id_file_name, 'w') do |f|
+    f.puts(id + 1)
+  end
 end
